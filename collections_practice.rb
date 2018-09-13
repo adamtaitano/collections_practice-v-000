525eba569dccfdb39786f3e7e872e02f6260d25e
Adam Taitano <adam.taitano@gmail.com>
2018-09-13 06:33:53 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,14 +9,8 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-    return array.sort do |a, b|
-      if a.length == b.length
-        0
-      elsif a.length < b.length
-        -1
-      elsif a.length > b.length
-        1
-      end
+    array.sort do |a, b|
+      a.length <==> b.length
     end
 end
 
